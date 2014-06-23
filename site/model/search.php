466a77f713e538d6185c07256dd35ffc83902b46
Vincent Sabourdy <vincent.sabourdy@viacesi.fr>
2014-06-23 11:23:15 +0200

    Fonction recherche finished

--- site/model/search.php
+++ site/model/search.php
@@ -3,23 +3,21 @@
 if(isset($_POST["search"])){
 
 		global $bdd;
-		$result = $bdd->prepare("SELECT * FROM product WHERE * LIKE \"".$keyword."\"");
+		$keyword = $_POST['search'];
+		$result = $bdd->prepare("SELECT * FROM product WHERE name LIKE \"%".$keyword."%\"");
 		$result->execute();
 
-		if($result){
-			
-			if(mysql_num_rows($result)==0) echo "<center><b>Pas de RÃ©sultat</b></center>"; else                {
-				echo "<center><b>RÃ©sultat(s)</b></center><br>";
-				while($row = mysql_fetch_array($result)){
-					echo $row["titre"];
-					$url = $row["url"];
-					echo " | < a href=\"$url\">$url</a>";
-				}
 
-				mysql_free_result($result);
-			}
+		
+		while($row = $result->fetch()){
+			echo $row["name"];
+			$url = $row["price"];
+			echo " | <a href=\"$url\">$url</a>";
 
+			echo "<br>";
 		}
+			
+		
 }
 	
 ?>
@@ -28,11 +26,11 @@ if(isset($_POST["search"])){
          
          <table>
             <tr>
-               <td><label for="search"><strong>Nom de compte</strong></label></td>
+               <td><label for="search"><strong>Search</strong></label></td>
                <td><input type="text" name="search" id="search"/></td>
             </tr>  
          </table>
-         <input type="submit" name="search" value="Rechercher"/>
+         <input type="submit" name="research"/>
       
       </form>
 
