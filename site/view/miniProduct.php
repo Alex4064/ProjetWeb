a8ad580246053e4500bd677280b58eca28b1f3bd
Vincent Sabourdy <vincent.sabourdy@viacesi.fr>
2014-06-23 16:41:56 +0200

    Mise en place Bootstrap et miniature produit

---
+++ site/view/miniProduct.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>Product</title>
+	<meta charset="utf-8">
+	<link href="style.css" rel="stylesheet">
+	<link href="css\bootstrap.css" rel="stylesheet">
+</head>
+
+<body>
+	<div class="content">
+		<div class="top"></div>
+
+		<div class="middle">
+
+<div class="row">
+  <div class="col-sm-2 col-sd-2">
+    <div class="thumbnail">
+      <img src="testpicture.jpg" alt="...">
+      <div class="caption">
+       <div class="name"> <h3>showName()</h3></div>
+        
+        <div class="quantity">showQuantity()</div> 
+		<div class="price"> showPrice() </div>
+        <p><a href="#" class="btn btn-primary" role="button">Details</a> <a href="#" class="btn btn-default addToBasket" role="button">Add to basket</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+		</div>
+
+		<div class="bottom"></div>
+	</div>
+
+
+</body>
+</html>
